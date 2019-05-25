424bea2ced3fdc31e8420d4b85945e2b44161bba
Alex <alexbrodriguez@gmail.com>
2019-05-25 12:31:11 -0400

    Just starting the project, no work yet

---
+++ SurplusApp/SurplusApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SurplusApp
+//
+//  Created by Alex Rodriguez on 5/25/19.
+//  Copyright © 2019 Alex Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
